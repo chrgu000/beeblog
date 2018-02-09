3443aef2d8efbe70295cf32419304a68b80eaeb0
ukinhappy <ukinhappy@163.com>
2018-02-09 15:33:06 +0800

    自动创建目录

--- main.go
+++ main.go
@@ -10,16 +10,18 @@ import (
 )
 
 func main() {
-	logs.EnableFuncCallDepth(true)
-	beego.SetLevel(beego.LevelDebug)
-	beego.SetLogger("file", `{"filename":"logs/beeblog.log"}`)
 
-	// 启动 beego
+	// 创建日志文件和附件目录
 	err := os.Mkdir("logs", os.ModePerm)
 	err = os.Mkdir("attachment", os.ModePerm)
 	if err != nil {
 		beego.Error(err)
 	}
+
+	logs.EnableFuncCallDepth(true)
+	beego.SetLevel(beego.LevelDebug)
+	beego.SetLogger("file", `{"filename":"logs/beeblog.log"}`)
+
 	beego.Run()
 
 }
